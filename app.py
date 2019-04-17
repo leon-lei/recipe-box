31188bd0297e21dbaaa68d434b9d74f64c1b9c8b
Leon Lei <leonlei35t@gmail.com>
2019-04-17 18:11:56 -0400

    Add: flask cors to pipfile and app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 
 # Init app
 app = Flask(__name__)
+CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database
@@ -84,7 +86,7 @@ def update_recipe(id):
     return recipe_schema.jsonify(recipe)
 
 
-@app.route("/recipe/<int:id>")
+@app.route('/recipe/<int:id>')
 def recipe(id):
     recipe = Recipe.query.get_or_404(id)
     return recipe_schema.jsonify(recipe)
